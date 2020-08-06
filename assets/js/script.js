1ae28b77766566ada1f1b0fa08a8261ee1526659
gsujanprdxn <sujang@prdxn.com>
2020-08-06 18:43:15 +0530

    js file is added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,23 @@
 
 */
 
+var arr =[12,14,16,18,20,26,27];
+var sum = prompt("enter a no:")
 
+for(i=0; i < arr.length; i++){
+    var first = arr[i];
+    // console.log(first);
+    for(j=i+1;  j < arr.length; j++){
+        var second = arr[j];
+        // console.log(second);
+        if(first+second == sum){
+            console.log("pair is "+first+ " and "+second);
+        }
+        else{
+            console.log("no pair")
+        }
+    }
+}
 
 
 
